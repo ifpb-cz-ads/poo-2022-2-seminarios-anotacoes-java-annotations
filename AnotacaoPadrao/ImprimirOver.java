f23ffae828c28ec7029cf26b992ebd46584cf4cb
wandsoader1 <wanderson.matheus@academico.ifpb.edu.br>
2023-01-31 21:21:53 -0300

    adicionando anotação padrão

---
+++ AnotacaoPadrao/ImprimirOver.java
@@ -0,0 +1,8 @@
+package AnotacaoPadrao;
+
+public class ImprimirOver extends Imprimir{
+    @Override
+    void imprimir(){
+        System.out.println("Método sobrescrito.");
+    }
+}
---
+++ AnotacaoPadrao/Teste.java
@@ -0,0 +1,9 @@
+package AnotacaoPadrao;
+public class Teste {
+    @SuppressWarnings("deprecation")
+	public static void main(String args[]){
+		Imprimir obj = new Imprimir();
+		obj.imprimir();
+		obj.imprimirDeprecated();
+	}
+}
